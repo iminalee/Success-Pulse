5dfaa9339e7389a7f456c2c1531730786d07a4da
iminalee <iam.minalee@gmail.com>
2026-01-04 11:30:54 +0900

    Create supabaseClient.js

---
+++ src/supabaseClient.js
@@ -0,0 +1,14 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://ihhfgoqpsubjdqlytzvs.supabase.co"; // 이미지에 있던 URL
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImloaGZnb3Fwc3ViamRxbHl0enZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxMDYxMDEsImV4cCI6MjA4MjY4MjEwMX0.1ZtWo4LiiOOJIFyKyvhPNXFwrvUgGeMTKTNp39kz61M"; // 이미지에 있던 Key
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    storageKey: "pulse-auth-token",
+    storage: window.localStorage,
+    autoRefreshToken: true,
+  },
+});
